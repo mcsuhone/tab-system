c81e49aa68c7a1505b333314eb1f7623dca3f996
mcsuhone <mikko.e.suhonen@gmail.com>
2024-07-04 17:51:21 +0300

    Temp: Working on forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import { Input } from '@/components/ui/input'
+import db from '@/modules/db'
 
 export default function Home() {
+  const addUser = async () => {
+    'use server'
+    const user = await db.user.create({
+      data: {
+        id: 155,
+        name: 'Mikko'
+      }
+    })
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h2 className="text-4xl p-14">Welcome to tab</h2>
