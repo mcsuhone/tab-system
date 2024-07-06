b411a8cccff641c30df9705437968a8205141c74
Mikko <mikko.e.suhonen@gmail.com>
2024-07-06 13:23:15 +0300

    UI Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
 'use client'
 
-import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage, FormDescription} from '@/components/ui/form'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  FormDescription
+} from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { zodResolver } from "@hookform/resolvers/zod"
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { z } from "zod"
+import { z } from 'zod'
 
 import { User } from '@prisma/client'
 import { useRouter } from 'next/navigation'
 
 const formSchema = z.object({
-  user_id: z.string().min(1).max(3),
+  user_id: z.string().min(1).max(3)
 })
 
 export default function Home() {
@@ -20,10 +28,10 @@ export default function Home() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      user_id: "",
-    },
+      user_id: ''
+    }
   })
- 
+
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     const response = await fetch(`/api/users/${values.user_id}`, {
       method: 'GET',
@@ -42,23 +50,25 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h2 className="text-4xl p-14">Welcome to tab</h2>
+      <h2 className="text-3xl p-14">Tervetuloa piikkiin</h2>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 justify-centered'>
-          <FormField 
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col space-y-4 items-center"
+        >
+          <FormField
             control={form.control}
-            name='user_id'
-            render={({field}) => (
+            name="user_id"
+            render={({ field }) => (
               <FormItem>
-                <FormLabel>
-                  User
-                </FormLabel>
+                <FormLabel>Käyttäjä</FormLabel>
                 <FormControl>
-                  <Input placeholder='***' {...field}/>
+                  <Input placeholder="***" {...field} />
                 </FormControl>
               </FormItem>
-            )}/>
-            <Button type='submit'>Submit</Button>
+            )}
+          />
+          <Button type="submit">Kirjaudu sisään</Button>
         </form>
       </Form>
     </main>
