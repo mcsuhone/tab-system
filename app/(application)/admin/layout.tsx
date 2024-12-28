7c08d9ed702ddd554b73911cd00cc6bd706f26c3
mcsuhone <mikko.e.suhonen@gmail.com>
2024-12-29 01:42:12 +0200

    Add margin below admin navbar

--- app/(application)/admin/layout.tsx
+++ app/(application)/admin/layout.tsx
@@ -10,7 +10,7 @@ export default async function AdminLayout({
     <PageContainer>
       <div className="flex flex-col h-full w-full overflow-hidden">
         <AdminNavbar />
-        <div className="flex-1 min-h-0 overflow-hidden">{children}</div>
+        <div className="flex-1 mt-4 min-h-0 overflow-hidden">{children}</div>
       </div>
     </PageContainer>
   )
